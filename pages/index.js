b2cfd0b9e83c97c6a86e6f0fe9fdf13ba430bfa7
mxylovezh <18322276773@163.com>
2020-06-25 10:58:21 +0800

    完成ant的按需引入

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Router from 'next/router'
 import React from 'react'
+import { Button } from 'antd'
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
+      <div><Button>我是按钮</Button></div>
     </>
   )
 }
